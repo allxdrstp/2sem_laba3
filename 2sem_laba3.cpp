7dd3f2deaf0b04f8268061ca251a2f4ea30783d5
allxdrstp <145099647+allxdrstp@users.noreply.github.com>
2024-03-28 20:19:32 +0300

    Add files via upload

---
+++ 2sem_laba3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+class VectorOnThePlane {
+protected:
+    double x, y;
+public:
+    VectorOnThePlane(double x, double y) {
+        this->x = x;
+        this->y = y;
+    }
+    virtual void print() {
+        cout << "x= " << x << " y= " << y << " ";
+    }
+};
+class VectorInThreeSpace : public VectorOnThePlane {
+    double z;
+public:
+    VectorInThreeSpace(double x, double y, double z) : VectorOnThePlane(x, y) {
+        this->z = z;
+    }
+    void print() {
+        VectorOnThePlane::print();
+        cout << " z= " << z << endl;
+    }
+};
+int main()
+{
+    VectorOnThePlane vector1(2, 3);
+    VectorInThreeSpace vector2(1, 2, 3);
+    vector1.print();
+    vector2.print();
+    VectorOnThePlane* vector1Ptr = new VectorOnThePlane(4, 6);
+    vector1Ptr->print();
+    VectorInThreeSpace* vector2Ptr = new VectorInThreeSpace(7, 8, 9);
+    vector2Ptr->print();
+    return 0;
+}
